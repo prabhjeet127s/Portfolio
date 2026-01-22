06b5f70fbae08c86c0f7d49d6c4c8208eb137de6
prabhjet singh <prabhjeet127.s@gmail.com>
2026-01-22 18:47:00 +0530

    fixing

--- src/components/Skillsdesc.jsx
+++ src/components/Skillsdesc.jsx
@@ -6,14 +6,14 @@ import React from 'react'
 const Skillsdesc = ({ skill }) => {
     return (
 
-        <div className='p-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-20 mx-50 m-10 place-items-center '>
+        <div className='p-10 grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-4 gap-20 mx-50 m-10 place-items-center '>
             {skill.map((value) => (
 
-                <div className='w-[11rem]     h-[2.2rem]  border-1 bg-gray-200 rounded-2xl overflow-hidden' >
+                <div className='w-[13rem]    h-auto border-1 bg-gray-200 rounded-2xl overflow-hidden' >
 
                     <div
                         style={{ width: `${value.experience}%`, backgroundColor: value.color }}
-                        className={` h-full bg-green-900 px-3 pt-1 shadow-black shadow-lg text-white`} >{value.Title}</div>
+                        className={` h-full py-8 bg-green-900 px-3 pt-1 shadow-black shadow-lg text-white`} >{value.Title}</div>
 
                 </div>
 
